2d08e8c0d88c3ab4efe11a53f59d8e2416ad714b
le0-n <farrux.farxodov2000@gmail.com>
2023-03-10 18:50:42 +0500

    v1.0

---
+++ main.js
@@ -0,0 +1,8 @@
+let centerList = document.getElementById('centerList')
+
+function burgMenu(){
+    centerList.style.transform = 'translate(0, 0%)'
+}
+function btnCloser(){
+    centerList.style.transform = 'translate(0, -100%)'
+}
